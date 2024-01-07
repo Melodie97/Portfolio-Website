46e34f338ef228e2e8c52f4fa606bd73d8eea743
Barry <ezeanibarry1@gmail.com>
2024-01-08 00:19:41 +0100

    updated index.html, style.css and script.js

--- script.js
+++ script.js
@@ -10,89 +10,136 @@
   <div>
     <img src="assets/Data Analytics/alexander-shatov-k1xf2D7jWUs-unsplash.jpg" height="150" width="180" />
   </div>
-  <div></div>
+  <div><a href="#"><h5>Twitter User(WeRateDogs) Analytics in Python</h5></a>
+  <p> Performed Analysis on twitter user (WeRateDogs) using data gotten from different independent
+     sources(API, URL, and csv file).</p></div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/covid-19.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div><a href="#"><h5>Covid19 SQL Analytics</h5></a>
+  <p>An Exploratory Data Analysis on a covid19 dataset using SQL</p></div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/jay-wennington-N_Y88TWmGwA-unsplash.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div><a href="#"><h5>Restaurant Analytics in PowerBI</h5></a>
+  <p>Summary Dashboard in PowerBI showing insights into a restaurant's orders and sales in a quarter.</div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/Revenue Dashboard.png" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div><a href="#"><h5>Revenue and Margin Analytics in Tableau</h5></a>
+  <p>Summary Dashboard in Tableau showing insights into a store's sales, highlighting the store's revenue and
+    gross margin.</p></div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/william-hook-9e9PD9blAto-unsplash.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div><a href="#"><h5>Customer Analytics on Mobile App Feature Usage in SQL</h5></a>
+  <p>Customers were grouped into frequency buckets based on how often they used a specific feature in a mobile app.
+    The Analytics aims to aid Targeted Ad Campaigns. </p></div>
 </div>`;
   
   const allHTML = `<div class="grid-item">
   <div>
     <img src="assets/Data Analytics/alexander-shatov-k1xf2D7jWUs-unsplash.jpg" height="150" width="180" />
   </div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Twitter User(WeRateDogs) Analytics in Python</h5></a>
+    <p> Performed Analysis on twitter user (WeRateDogs) using data gotten from different independent
+       sources(API, URL, and csv file).</p>
+  </div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/covid-19.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Covid19 SQL Analytics</h5></a>
+    <p>An Exploratory Data Analysis on a covid19 dataset using SQL</p>
+  </div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/jay-wennington-N_Y88TWmGwA-unsplash.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Restaurant Analytics in PowerBI</h5></a>
+    <p>Summary Dashboard in PowerBI showing insights into a restaurant's orders and sales in a quarter.</p>
+  </div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/Revenue Dashboard.png" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Revenue and Margin Analytics in Tableau</h5></a>
+    <p>Summary Dashboard in Tableau showing insights into a store's sales, highlighting the store's revenue and
+      gross margin.</p>
+  </div>
 </div>
 <div class="grid-item">
   <div>
     <img src="assets/Data Analytics/william-hook-9e9PD9blAto-unsplash.jpg" alt="" height="150" width="180">
   </div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Customer Analytics on Mobile App Feature Usage in SQL</h5></a>
+    <p>Customers were grouped into frequency buckets based on how often they used a specific feature in a mobile app.
+      The Analytics aims to aid Targeted Ad Campaigns. </p>
+  </div>
 </div>
 <div class="grid-item">
   <div><img src="assets/Data Engineering/ETL Project.png" alt="" height="150" width="180"></div>
-  <div></div>
+  <div>
+    <a href="#"><h5>ETL Project in Python</h5></a>
+    <p>Extracted data from a csv file, Transformed the data (cleaning, removal of duplicates and some missing values), and
+      Loaded it into a Postgres Database.
+    </p>
+  </div>
 </div>
 <div class="grid-item">
   <div><img src="assets/Data Science/BOE.jpg" alt="" height="150" width="180"></div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Bank Loan Classification in Python</h5></a>
+    <p> Built several ML models to determine the one that best classifies bad loans based on the available features.</p>
+  </div>
 </div>
 <div class="grid-item">
   <div><img src="assets/Data Science/mojahid-mottakin-f2MlXoFMy54-unsplash.jpg" height="150" width="180"></div>
-  <div></div>
-</div>
-</div>`;
-
-  const dataEngineeringHTML = `<div class="grid-item">
-  <div><img src="assets/Data Engineering/ETL Project.png" alt="" height="150" width="180"></div>
-  <div></div>
+  <div>
+    <a href="#"><h5>Custom Chatbot for Nasa Webpage</h5></a>
+    <p> Built a custom Chatbot using an openAI LLM. The process involved scraping the webpage and fine tuning the 
+      model with the data.
+    </p>
+  </div>
 </div>`;
 
   const dataScienceHTML = `<div class="grid-item">
   <div><img src="assets/Data Science/BOE.jpg" alt="" height="150" width="180"></div>
-  <div></div>
+  <div><a href="#"><h5>Bank Loan Classification in Python</h5></a>
+  <p> Built several ML models to determine the one that best classifies bad loans based on the available features.</p></div>
 </div>
 <div class="grid-item">
   <div><img src="assets/Data Science/mojahid-mottakin-f2MlXoFMy54-unsplash.jpg" height="150" width="180"></div>
-  <div></div>
+  <div><a href="#"><h5>Custom Chatbot for Nasa Webpage</h5></a>
+  <p> Built a custom Chatbot using an openAI LLM. The process involved scraping the webpage and fine tuning the 
+    model with the data.
+  </p></div>
+</div>`;
+
+const dataEngineeringHTML = `<div class="grid-item">
+<div><img src="assets/Data Engineering/ETL Project.png" alt="" height="150" width="180"></div>
+<div>
+  <a href="#"><h5>ETL Project in Python</h5></a>
+  <p>Extracted data from a csv file, Transformed the data (cleaning, removal of duplicates and some missing values), and
+    Loaded it into a Postgres Database.
+  </p>
+</div>
 </div>`;
 
   dataAnalyticsBtn.addEventListener("click", function () {
